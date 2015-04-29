607b14d34c8be7021edc5594c4fefa8c2489fd55
chenlizhan <chung1350@hotmail.com>
2015-04-29 22:23:34 +0800

    Stu mistake

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ getAllMessages();
 submit.addEventListener('click', function(e) {
     e.preventDefault();
     var sender = getCookie('username');
-    saveMessage(sender message.value);
+    saveMessage(sender, message.value);
     pool.innerHTML += "<div class=\"col-md-8\">" + sender + ': ' + message.value + "</div><div class=\"col-md-4\">" + time_diff(Date.now(), Date.now()) + "分鐘前</div>";
     message.value = "";
 });
