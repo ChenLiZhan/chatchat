9a761c79c8786d1565280e1f66919e01c2f4301d
chenlizhan <chung1350@hotmail.com>
2015-04-29 16:35:16 +0800

    Different login way

--- js/facebook.js
+++ js/facebook.js
@@ -17,12 +17,24 @@ window.fbAsyncInit = function() {
     //    your app or not.
     //
     // These three cases are handled in the callback function.
-    console.log(FB.getLoginStatus());
     $('#about').click(function() {
         FB.api("/me", function(response) {
             if (response && !response.error) {
                 /* handle the result */
                 console.log(response);
+            }
+        });
+    });
+
+    $('#login').click(function() {
+        FB.login(function(response) {
+            if (response.authResponse) {
+                console.log('Welcome!  Fetching your information.... ');
+                FB.api('/me', function(response) {
+                    console.log('Good to see you, ' + response.name + '.');
+                });
+            } else {
+                console.log('User cancelled login or did not fully authorize.');
             }
         });
     });
