d8fc1be22761b9aebcf2a9a6d204876ba5a331fa
chenlizhan <chung1350@hotmail.com>
2015-04-29 22:15:13 +0800

    Get cookie function

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ getAllMessages();
 submit.addEventListener('click', function(e) {
     e.preventDefault();
     saveMessage(message.value);
-    console.log(document.cookie);
+    console.log(getCookie('username'));
     pool.innerHTML += "<div class=\"col-md-8\">" + message.value + "</div><div class=\"col-md-4\">" + time_diff(Date.now(), Date.now()) + "分鐘前</div>";
     message.value = "";
 });
@@ -18,3 +18,14 @@ refresh.addEventListener('click', function(e) {
     pool.innerHTML = "";
     getAllMessages();
 });
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
